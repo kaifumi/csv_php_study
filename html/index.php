360bbce2285326935c66540a9bdd690ded113fce
kaifumi <kaifumi1001@gmail.com>
2020-08-12 19:15:31 +0900

    最初のコミット

---
+++ html/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>php7.4-apache</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+Hello World
+<?php phpinfo(); ?>
+</body>
+</html>
