cd9323d6e7a1c4608fd92e0ea6c595c75f05a8dc
kaifumi <kaifumi1001@gmail.com>
2020-08-13 13:52:29 +0900

    volume 領域の拡大

--- html/index.php
+++ html/index.php
@@ -9,15 +9,28 @@
 <body>
 <h1>ハローワールド</h1>
 <?php
+// echo  __DIR__ . "<br />";
+// echo './csv/AMU_PAY_SHOP_LIST.csv';
+// $row = 1;
+// $filepath = "../csv/AMU_PAY_SHOP_LIST.csv";
+// $file = file_get_contents($filepath);
+// $lines = str_getcsv($file, "\r\n");
+// foreach ($lines as $line) {
+//     $records[] = str_getcsv($line);
+// }
+
+// var_dump ($records);
+// var_dump ($file);
+
 $row = 1;
 // ファイルが存在しているかチェックする
-if (($handle = fopen("csv/AMU PAY SHOP LIST.csv", "r")) !== FALSE) {
+if (($handle = fopen("../csv/AMU_PAY_SHOP_LIST.csv", "r")) !== FALSE) {
     // 1行ずつfgetcsv()関数を使って読み込む
     while (($data = fgetcsv($handle))) {
         echo "${row}行目\n";
         $row++;
         foreach ($data as $value) {
-            echo "「${value}」\n";
+            echo "${value}\n";
         }
     }
     fclose($handle);
