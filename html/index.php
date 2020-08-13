99099c2d1af0c658786a780828b4eefc8c909400
kaifumi <kaifumi1001@gmail.com>
2020-08-13 09:58:53 +0900

    メモを追加

--- html/index.php
+++ html/index.php
@@ -7,6 +7,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+<h1>ハローワールド</h1>
+<?php
+$row = 1;
+// ファイルが存在しているかチェックする
+if (($handle = fopen("csv/AMU PAY SHOP LIST.csv", "r")) !== FALSE) {
+    // 1行ずつfgetcsv()関数を使って読み込む
+    while (($data = fgetcsv($handle))) {
+        echo "${row}行目\n";
+        $row++;
+        foreach ($data as $value) {
+            echo "「${value}」\n";
+        }
+    }
+    fclose($handle);
+}
 
+?>
 </body>
 </html>
