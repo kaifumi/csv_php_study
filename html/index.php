6befd9ccdd5bfe439ee2ffa821e5610e6826520f
kaifumi <kaifumi1001@gmail.com>
2020-08-14 14:21:38 +0900

    includeを追加

--- html/index.php
+++ html/index.php
@@ -8,66 +8,10 @@
 </head>
 <body>
 <?php
-$PREFCTUR_FILEPATH = "../csv/prefecture_list.csv";
-$SHOP_FILEPATH = "../csv/shop_list.csv";
+include('prefecture.php');
+include('shop.php');
 
-// csvファイルを読み込み
-
-$prefecture_file = file_get_contents($PREFCTUR_FILEPATH);
-$shop_file = file_get_contents($SHOP_FILEPATH);
-$prefecture_lines = str_getcsv($prefecture_file, "\r\n");
-$shop_lines = str_getcsv($shop_file, "\r\n");
-
-// csvファイルを配列データに置き換える
-
-foreach ($prefecture_lines as $line) {
-    $prefecture_records[] = preg_split("/,/", $line);
-}
-foreach ($shop_lines as $line) {
-    $shop_records[] = preg_split("/,/", $line);
-}
-
-// 地区ごとに振り分ける
-function area_array($prefecture_records, $area_name) {
-    foreach ($prefecture_records as $record) {
-        if ($record[2] == $area_name) {
-            $area[] = $record;
-        }
-    }
-    return $area;
-}
-
-// 地区ごとに配列を用意
-
-$HOKAIDO_TOHOKU_records = area_array($prefecture_records, "北海道・東北");
-$KANTO_records = area_array($prefecture_records, "関東");
-$HOKURIKU_KOUSINETU_records = area_array($prefecture_records, "北陸・甲信越");
-$TOKAI_records = area_array($prefecture_records, "東海");
-$KINKI_records = area_array($prefecture_records, "近畿");
-$CHUGOKU_records = area_array($prefecture_records, "中国");
-$SHIKOKU_records = area_array($prefecture_records, "四国");
-$KYUSYU_OKINAWA_records = area_array($prefecture_records, "九州・沖縄");
-
-
-// 配列の中身を出力
-
-foreach ($prefecture_records as $record) {
-    echo $record[0] . "<br/>";
-    echo $record[1] . "<br/>";
-    echo $record[2] . "<br/>";
-    echo "<br/>";
-}
-
-foreach ($shop_records as $record) {
-    echo $record[1] . "<br/>";
-    echo $record[2] . "<br/>";
-    echo $record[3] . "<br/>";
-    echo $record[4] . "<br/>";
-    echo $record[5] . "<br/>";
-    echo $record[6] . "<br/>";
-    echo $record[7] . "<br/>";
-    echo "<br/>";
-}
+var_dump($per_district_records);
 
 ?>
 </body>
